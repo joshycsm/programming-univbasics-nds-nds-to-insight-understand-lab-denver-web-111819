1fc68b5762c9da57db1ad8d9a66381e6806fe14c
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-11-04 22:03:12 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,12 +8,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-   ss_movies = directors_database[0][:movies]
-  index = 0
+  # ss_movies = directors_database[0][:movies]
+  # index = 0
 
-  while index < ss_movies.length do
-    titles = ss_movies[index][:title]
-    puts titles
-    index +=1
-  end
+  # while index < ss_movies.length do
+  #   titles = ss_movies[index][:title]
+  #   puts titles
+  #   index +=1
+  # end
 end
